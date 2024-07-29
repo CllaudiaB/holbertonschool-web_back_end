f557529fa9f0b1aaf689172df9bd1f9dff552a68
CllaudiaB <buraanna27@gmail.com>
2024-07-29 11:40:03 +0200

    update

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -5,7 +5,7 @@
 def sum_list(input_list: "list[float]") -> float:
     """takes a list input_list of floats as argument
     and returns their sum as a float"""
-    sum = 0
+    sum: float = None
     for number in input_list:
         sum += number
 
