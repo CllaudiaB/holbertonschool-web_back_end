b8687142849b58ff3d32ddfb35ca2ae7c39f68b3
CllaudiaB <buraanna27@gmail.com>
2024-09-23 12:35:33 +0200

    add comment

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""Returns the log message obfuscated"""
 import re
 from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str , message: str, separator: str) -> str:
+    """Returns the log message obfuscated"""
     parts = message.split(separator)
     for idx, text in enumerate(parts):
         if text.startswith(tuple(fields)):
