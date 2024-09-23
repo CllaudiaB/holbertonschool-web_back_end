3e9cdb053854126f48d04140ad30566d437772f3
CllaudiaB <buraanna27@gmail.com>
2024-09-23 13:03:29 +0200

    fix style

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,8 +4,9 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     """Returns the log message obfuscated"""
     parts = message.split(separator)
     for idx, text in enumerate(parts):
