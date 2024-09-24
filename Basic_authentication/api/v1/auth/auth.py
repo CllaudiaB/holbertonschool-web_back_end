36a2d2d01a90e0a48235592be6627af7ad2598af
CllaudiaB <buraanna27@gmail.com>
2024-09-24 14:30:09 +0200

    task 4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,9 +8,21 @@ from flask import request
 
 class Auth:
     """Basic authentication"""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Public method that return False"""
-        return False
+        """Define which routes don't need authentication"""
+        if path is None:
+            return True
+        if excluded_paths == [] or excluded_paths is None:
+            return True
+        if path in excluded_paths:
+            return False
+
+        if (
+            path == "/api/v1/status" or path == "/api/v1/status/"
+        ) and "/api/v1/status/" in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Public method that return None"""
