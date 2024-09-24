7f070abf42f741ae7247f871b247f1b0cf6aab08
CllaudiaB <buraanna27@gmail.com>
2024-09-24 10:16:14 +0200

    task 5

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Encrypting passwords"""
+
+import bcrypt
+from typing import ByteString
+
+def hash_password(password: str) -> ByteString:
+    """Return hashed password"""
+    return bcrypt.hashpw(b"{password}", bcrypt.gensalt())
