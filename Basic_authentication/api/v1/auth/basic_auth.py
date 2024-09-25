737533ac5957e6b453df7b64223e9a32002b4da1
CllaudiaB <buraanna27@gmail.com>
2024-09-25 12:03:42 +0200

    task 9

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,7 +13,7 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(
             self, authorization_header: str
     ) -> str:
-        """Returns the Base64 part of the Authorization
+        """Return the Base64 part of the Authorization
             header for a Basic Authentication"""
         if authorization_header is None:
             return None
@@ -27,7 +27,7 @@ class BasicAuth(Auth):
     def decode_base64_authorization_header(
         self, base64_authorization_header: str
     ) -> str:
-        """Returns the decoded value of a Base64
+        """Return the decoded value of a Base64
            string base64_authorization_header"""
         if base64_authorization_header is None:
             return None
@@ -36,8 +36,23 @@ class BasicAuth(Auth):
         try:
             valid = base64.b64decode(
                 base64_authorization_header, validate=True
-            )
+                )
             if valid:
                 return valid.decode("utf-8")
         except binascii.Error:
             return None
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """Return the user email and password
+           from the Base64 decoded value"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ":" in decoded_base64_authorization_header:
+            res = decoded_base64_authorization_header.split(":")
+            return tuple(res)
+        else:
+            return None, None
