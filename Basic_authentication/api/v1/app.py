f12bf8c9a1e830b80f11cb99002390a3f3f2997c
CllaudiaB <buraanna27@gmail.com>
2024-09-25 10:03:42 +0200

    fix: style

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -50,7 +50,11 @@ def before_request():
     if auth is None:
         return
     if not auth.require_auth(
-        request.path, ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"]
+        request.path, [
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/"
+            ]
     ):
         return
     if auth.authorization_header(request) is None:
