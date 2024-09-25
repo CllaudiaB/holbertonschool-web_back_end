f6d7eaf98d9aba8bfe47f826937ee14e03d203fd
CllaudiaB <buraanna27@gmail.com>
2024-09-25 11:05:35 +0200

    task 8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,14 +3,13 @@
 
 import re
 from api.v1.auth.auth import Auth
+import base64, binascii
 
 
 class BasicAuth(Auth):
     """Basic auth"""
 
-    def extract_base64_authorization_header(
-            self, authorization_header: str
-    ) -> str:
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """Returns the Base64 part of the Authorization
             header for a Basic Authentication"""
         if authorization_header is None:
@@ -21,3 +20,18 @@ class BasicAuth(Auth):
             return
         value = re.search(r"[^\s]*$", authorization_header)
         return value.group()
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """Returns the decoded value of a Base64
+           string base64_authorization_header"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            valid = base64.b64decode(base64_authorization_header, validate=True)
+        except binascii.Error:
+            return None
+        return valid.decode("utf-8")
