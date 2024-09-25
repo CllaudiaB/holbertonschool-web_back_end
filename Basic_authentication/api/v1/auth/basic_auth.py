6d68c8e7c8404340a3db9d2c8ec84fb8a6a7d91f
CllaudiaB <buraanna27@gmail.com>
2024-09-25 10:49:17 +0200

    fix: style

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,8 +8,11 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Basic auth"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        """Returns the Base64 part of the Authorization header for a Basic Authentication"""
+    def extract_base64_authorization_header(
+            self, authorization_header: str
+    ) -> str:
+        """Returns the Base64 part of the Authorization
+            header for a Basic Authentication"""
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
