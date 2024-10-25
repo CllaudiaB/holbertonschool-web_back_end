0bc08369d8fbbefb14e7a08c7539a948a18b7fa8
CllaudiaB <buraanna27@gmail.com>
2024-10-25 10:59:45 +0200

    add type

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Create user"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
