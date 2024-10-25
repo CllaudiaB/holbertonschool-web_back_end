0a127c6ae37f8dcc46d6381b78ab862dea537b49
CllaudiaB <buraanna27@gmail.com>
2024-10-25 11:29:20 +0200

    Find user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -38,3 +40,15 @@ class DB:
         self._session.commit()
 
         return user
+
+    def find_user_by(self, **kwargs: str) -> User:
+        """Find user"""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except NoResultFound:
+            raise NoResultFound
+        except InvalidRequestError:
+            raise InvalidRequestError
