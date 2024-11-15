77f57eae793c16aff75dadc121e560264ac65f3c
CllaudiaB <buraanna27@gmail.com>
2024-11-15 11:33:03 +0100

    task 1

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -8,13 +8,14 @@ from flask_babel import Babel
 
 class Config():
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-app.config.from_object('app.config.settings')
+app.config.from_object(Config)
 
-babel = Babel(app, locale_selector=Config.LANGUAGES[0],
-              timezone_selector="UTC")
+babel = Babel(app)
 
 
 @app.route("/")
