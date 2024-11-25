63ff4703f7dfd42791602b0a23b104495287a822
CllaudiaB <buraanna27@gmail.com>
2024-11-25 11:05:42 +0100

    task 0

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -12,7 +12,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: str | bytes | int | float) -> str:
         """Store data in Redis"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
