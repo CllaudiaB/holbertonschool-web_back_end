7bbc37e537884f19be968d395ce435bfb1e2e3ac
CllaudiaB <buraanna27@gmail.com>
2024-11-25 12:23:23 +0100

    task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Redis exercices"""
 import redis
-from typing import Union
+from typing import Callable, Optional, Union
 import uuid
 
 
@@ -18,3 +18,19 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> \
+            Union[str, bytes, int, float]:
+        """Get value"""
+        value = self._redis.get(key)
+        if key and fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Convert str"""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """Convert int"""
+        return self.get(key, int)
