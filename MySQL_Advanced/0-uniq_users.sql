5c6b9bec350ab17424832e9495b81726358d8757
CllaudiaB <buraanna27@gmail.com>
2024-11-28 12:00:33 +0100

    task 0

---
+++ MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,11 @@
+-- task 0
+-- create table
+
+DROP TABLE IF EXISTS users ;
+
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
