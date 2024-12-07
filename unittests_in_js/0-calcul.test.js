ff9c5ce91f660ab3ff726fc41a2d4c2eb6fcf11f
CllaudiaB <buraanna27@gmail.com>
2024-12-07 10:46:37 +0100

    task 0

---
+++ unittests_in_js/0-calcul.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const { calculateNumber } = require('./0-calcul.js');
+
+describe("calculateNumber Tests", function () {
+    it("Retourn 4 for calculateNumber(1, 3)", function () {
+      assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+    it("Retourn 5 for calculateNumber(1, 3.7)", function () {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+      });
+    it("Retourn 5 for calculateNumber(1.2, 3.7)", function () {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+    it("Retourn 5 for calculateNumber(1.5, 3.7)", function () {
+        assert.strictEqual(calculateNumber(1.5, 3.7), 5);
+      });
+});
