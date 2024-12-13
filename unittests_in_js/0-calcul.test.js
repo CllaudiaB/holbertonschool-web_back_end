9a355ec6c75c00e037fa70bb45690a6e9bcd414a
CllaudiaB <buraanna27@gmail.com>
2024-12-13 09:22:57 +0100

    tests

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -2,16 +2,12 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe("calculateNumber Tests", function () {
-    it("Retourn 4 for calculateNumber(1, 3)", function () {
+    it("Retourn sum", function () {
       assert.strictEqual(calculateNumber(1, 3), 4);
+      assert.strictEqual(calculateNumber(1, 3.7), 5);
+      assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+      assert.strictEqual(calculateNumber(-1.3, -4.5), -5);
+      assert.strictEqual(calculateNumber(0.4, 0.6), 1);
     });
-    it("Retourn 5 for calculateNumber(1, 3.7)", function () {
-        assert.strictEqual(calculateNumber(1, 3.7), 5);
-      });
-    it("Retourn 5 for calculateNumber(1.2, 3.7)", function () {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    });
-    it("Retourn 6 for calculateNumber(1.5, 3.7)", function () {
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-      });
 });
