6d0565bef22d809523c1b14422ed9bcddc0871b1
CllaudiaB <buraanna27@gmail.com>
2024-12-13 10:30:48 +0100

    task 2

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -1,18 +1,17 @@
-const chai = require('chai');
-const { expect } = chai;
-const calculateNumber = require("./2-calcul.js");
+const expect = require("chai").expect;
+const calculateNumber = require("./2-calcul_chai.js");
 
 describe("calculateNumber Tests", function () {
     it("Retourn 6 for calculateNumber('SUM', 1.4, 4.5)", function () {
-        expect(calculateNumber("SUM", 1.4, 4.5)).to.equal(6);
+        expect(calculateNumber("SUM", 1.4, 4.5)).be.equal(6);
     });
     it("Retourn -4 for calculateNumber('SUBTRACT', 1.4, 4.5)", function () {
-        expect(calculateNumber("SUBTRACT", 1.4, 4.5)).to.equal(-4);
+        expect(calculateNumber("SUBTRACT", 1.4, 4.5)).be.equal(-4);
       });
     it("Retourn 0.2 for calculateNumber('DIVIDE', 1.4, 4.5)", function () {
-        expect(calculateNumber("DIVIDE", 1.4, 4.5)).to.equal(0.2);
+        expect(calculateNumber("DIVIDE", 1.4, 4.5)).be.equal(0.2);
     });
     it("Retourn 'Error' for calculateNumber('DIVIDE', 1.4, 0)", function () {
-        expect(calculateNumber("DIVIDE", 1.4, 0)).to.equal("Error");
+        expect(calculateNumber("DIVIDE", 1.4, 0)).be.equal("Error");
     });
 });
