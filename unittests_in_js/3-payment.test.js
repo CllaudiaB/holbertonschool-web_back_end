6e14521bd701bc38c29aac055e025d565030fbed
CllaudiaB <buraanna27@gmail.com>
2024-12-13 12:58:40 +0100

    task 3

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -1,14 +1,14 @@
 const sinon = require("sinon");
-const expect = require("chai").expect;
+const expect = require("chai");
 const Utils = require("./utils");
 const sendPaymentRequestToApi = require("./3-payment");
 
 describe("sendPaymentRequestToApi Tests", function () {
   it("should call calculateNumber with correct arguments when sendPaymentRequestToApi is called", function () {
     sinon.spy(Utils, "calculateNumber");
-    sendPaymentRequestToApi(100, 20);
-    expect(Utils.calculateNumber("SUM", 100, 20).calledOnce).to.be
-    .true;
+    sendPaymentRequestToApi(100, 200);
+    expect(Utils.calculateNumber.withArgs("SUM", 100, 200).calledOnce).to.be
+      .true;
     Utils.calculateNumber.restore();
   });
 });
