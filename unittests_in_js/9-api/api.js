e0d6b22b67d31eac5ef5a850f6114d6e3106efae
CllaudiaB <buraanna27@gmail.com>
2024-12-14 13:19:09 +0100

    task 9

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -7,12 +7,11 @@ app.get('/', (req, res) => {
     res.send('Welcome to the payment system');
   });
   
-  app.get('/cart/:id(\\d+)', (req, res) => {
-    const cartId = req.params.id;
-    res.send(`Payment methods for cart ${cartId}`);
-  });
-  
-  app.listen(port, () => {
-    console.log('API available on localhost port 7865');
-  });
+app.get('/cart/:id(\\d+)', (req, res) => {
+const cartId = req.params.id;
+res.send(`Payment methods for cart ${cartId}`);
+});
 
+app.listen(port, () => {
+console.log('API available on localhost port 7865');
+});
