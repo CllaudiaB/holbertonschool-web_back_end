4db3d82f341052358fa6cfa9cf45042f31edad8f
CllaudiaB <buraanna27@gmail.com>
2024-12-14 11:50:49 +0100

    task 6

--- unittests_in_js/6-payment_token.test.js
+++ unittests_in_js/6-payment_token.test.js
@@ -3,18 +3,13 @@ const expect = require("chai").expect;
 
 describe('getPaymentTokenFromAPI', function () {
     it('should return a resolved promise when success is true', function (done) {
-        getPaymentTokenFromAPI(true)
-            .then((response) => {
-                expect(response).toEqual({ data: 'Successful response from the API' });
-                done();
-            })
-            .catch((error) => {
-                done(error);
-            });
+      getPaymentTokenFromAPI(true)
+        .then((data) => {
+          expect(data).to.deep.equal({
+            data: 'Successful response from the API',
+          });
+          done();
+        })
+        .catch((error) => done(error));
     });
-
-    it('should do nothing when success is false', function () {
-        const result = getPaymentTokenFromAPI(false);
-        expect(result).toBeUndefined();
-    });
-});
+  });
