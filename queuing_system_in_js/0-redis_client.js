74f65a211d7dc81518804072bf5006252ee06cbc
CllaudiaB <buraanna27@gmail.com>
2025-01-02 17:21:29 +0100

    task 1

---
+++ queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,10 @@
+const redis = require('redis');
+const client = redis.createClient();
+
+client.on('connect', function() {
+  console.log('Redis client connected to the server');
+});
+
+client.on("error", function(err){ 
+    console.log(`Redis client not connected to the server: ${err}`); 
+});
