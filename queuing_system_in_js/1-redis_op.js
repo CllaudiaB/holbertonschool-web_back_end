fd035327557c384ab2e34f7e290b6edd64b4ee8b
CllaudiaB <buraanna27@gmail.com>
2025-01-02 17:55:42 +0100

    task 2

--- queuing_system_in_js/1-redis_op.js
+++ queuing_system_in_js/1-redis_op.js
@@ -1,6 +1,14 @@
 const redis = require('redis');
 const client = redis.createClient();
 
+client.on('connect', function() {
+  console.log('Redis client connected to the server');
+});
+
+client.on("error", function(err){ 
+    console.log(`Redis client not connected to the server: ${err}`); 
+});
+
 function setNewSchool(schoolName, value) {
     client.set(schoolName, value, redis.print);
 };
@@ -9,13 +17,6 @@ function displaySchoolValue(schoolName) {
     client.get(schoolName, redis.print)
 };
 
-client.on('connect', function() {
-    console.log('Redis client connected to the server');
-    displaySchoolValue('Holberton');
-    setNewSchool('HolbertonSanFrancisco', '100');
-    displaySchoolValue('HolbertonSanFrancisco');
-});
-
-client.on("error", function(err){ 
-    console.log(`Redis client not connected to the server: ${err}`);
-});
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
