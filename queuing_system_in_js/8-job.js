bcdb3277a9bf0140a6a3b39bf93a4c5f5345bc2d
CllaudiaB <buraanna27@gmail.com>
2025-01-07 20:51:25 +0100

    task 10

---
+++ queuing_system_in_js/8-job.js
@@ -0,0 +1,27 @@
+export default function createPushNotificationsJobs(jobs, queue) {
+    if(!Array.isArray(jobs)) {
+        return done (Error ('Jobs is not an array'))
+    }
+
+    jobs.forEach((j) => {
+        let job = queue.create('push_notification_code_2', j)
+        .save((err) => {
+            if (!err) {
+                console.log(`Notification job created: ${job.id}`);
+            }
+        });
+    
+        job.on('complete', () => {
+            console.log(`Notification job ${job.id} completed`);
+        });
+    
+        job.on('failed', (err) => {
+            console.log(`Notification job ${job.id} failed: ${err}`);
+        });
+    
+        job.on('progress', (progress) => {
+            console.log(`Notification job ${job.id} ${progress}% complete`);
+        });
+    });
+
+};
